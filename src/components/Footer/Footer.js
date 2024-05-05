cf733392d39c4549fe6efba7cdbac75b24f94d9c
Vasileios Papastergios <papastva@csd.auth.gr>
2024-05-05 23:37:14 +0300

    Make footer more responsive

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -12,22 +12,24 @@ import Button from "../UI/Button";
 const Footer = (props) => {
   return (
     <footer className="bg-secondary bg-opacity-20 group py-6 mt-4 text-trietary-dark italic text-sm font-firaSans">
-    {/* <footer className="shadow-inner group py-3 text-trietary-dark italic text-sm font-firaSans"> */}
+      {/* <footer className="shadow-inner group py-3 text-trietary-dark italic text-sm font-firaSans"> */}
       <div className="container">
-        <div className="flex items-center justify-between">
-        <div className="footer-text  w-3/5 align-middle">
-          <div className="inline-block align-top pr-2">{copyrightSVG}</div>
-          Maria Kavouridou, 2023-2024. Developed by Bilpapster with{" "}
-          <div className="inline-block align-top pr-1 group-hover:text-red-600 group-hover:animate-heart-beat">{heartSVG}</div>, pride &
-          admiration for his life partner, using HTML5, TailwindCSS & ReactJS.
-        </div>
-        <Button
-          aClass="not-italic"
-          link="https://www.github.com/bilpapster"
-          target="_blank"
-          buttonText={content.buttonText}
-          svg={githubSVG}
-        />
+        <div className="grid grid-cols-12 gap-0">
+          <div className="col-span-12 md:col-span-8 my-auto p-0">
+            <div className="inline-block pr-2 align-middle">{copyrightSVG}</div>
+            Maria Kavouridou, 2023-2024. Developed by Bilpapster with{" "}
+            <div className="inline-block align-middle pr-1 group-hover:text-red-600 group-hover:animate-heart-beat">
+              {heartSVG}
+            </div>
+            , using HTML5, TailwindCSS & ReactJS.
+          </div>
+          <Button
+            aClass="not-italic col-span-12 md:col-span-4 mt-4 md:mt-0 w-fit justify-self-center md:justify-self-end"
+            link="https://www.github.com/bilpapster"
+            target="_blank"
+            buttonText={content.buttonText}
+            svg={githubSVG}
+          />
         </div>
       </div>
     </footer>
--- src/content/ContentFooter.js
+++ src/content/ContentFooter.js
@@ -1,9 +1,9 @@
-import content from './ContentPersonalInfo'
+import content from "./ContentPersonalInfo";
 
 const footer = {
-    copyright: `${content.name} ${content.surname}`,
-    text: 'All rights reserved. Developed by bilpapster with ♥, pride and admiration for his life partner. The website is still under construction and you are viewing a demo version, thanks for your understanding!',
-    buttonText: 'Developer'
+  copyright: `${content.name} ${content.surname}`,
+  text: "All rights reserved. Developed by bilpapster with ♥, pride and admiration for his life partner. The website is still under construction and you are viewing a demo version, thanks for your understanding!",
+  buttonText: "See developer",
 };
 
 export default footer;
