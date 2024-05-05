f5a4e19204ca0c3f9fb9b909bc9a220feffdc51d
Vasileios Papastergios <papastva@csd.auth.gr>
2024-05-05 14:59:03 +0300

    Make a small change in phrasing

--- src/content/ContentAbout.js
+++ src/content/ContentAbout.js
@@ -1,5 +1,5 @@
 const about = [
-  'I am a determined and ambitious individual with an unwavering passion for both athletics and academics. Seeking an athletic-academic scholarship, I am committed to pursuing my dreams of becoming a champion while excelling in my studies. As an athlete, I have numerous victories, specializing in distance running, from 5 kilometers to 30 kilometers. Running is not only my passion but also a fundamental aspect of my identity.',
+  'I am a determined and ambitious individual with an unwavering passion for both athletics and academics. Seeking an athletic-academic scholarship, I am committed to pursuing my dreams of becoming a champion while excelling in my studies. As an athlete, I have numerous victories, specializing in distance running, from 5 up to 30 kilometers. Running is not only my passion but also a fundamental aspect of my identity.',
 
   'Off the track, I am deeply involved in graphic design, after having gained some expertise (mostly with Adobe Photoshop and Illustrator). I am continually pursuing further education, so I can start venturing in some professional pursuits and gain some valuable experience. In academics, I maintain a strong focus on balancing  coursework with my athletic and extracurricular pursuits.',
 
